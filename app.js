a46e9b84c5c37e0774e20134be0d27a07cfe9797
Rebeca Molina <rebe.molina15@hotmail.com>
2022-08-09 20:04:36 -0300

    Arrays

--- app.js
+++ app.js
@@ -30,3 +30,33 @@ while (continuar === "No") {
 
     usuario = prompt("Ingrese el banco por el cual quiere consultar") 
 } 
+
+// ARRAYS 
+
+class Bancos {
+    constructor(id, banco, interes) {
+        this.id = id
+        this.banco = banco
+        this.interes = interes
+    }
+}
+
+const bancos = [
+    new Bancos (01, "Santander Rio", 1.12),
+    new Bancos (02, "Galicia", 2.40),
+    new Bancos (03, "Nacion", 1.90)
+]
+
+console.log(bancos)
+
+function agregarBancos() {
+    let id = Number( prompt("Ingrese número de ID del banco") )
+    let banco = prompt("Ingrese entidad bancaria")
+    let interes = Number ( prompt("Ingrese interés correspondiente al banco elegido") )
+
+    bancos.push(new Bancos (id, banco, interes))
+}
+
+
+agregarBancos()
+console.log(bancos)
